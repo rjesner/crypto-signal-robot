3502ff9b27dfbd5eb5e43c99586adeb68c45991b
Rodrigo Jesner Silva Trindade <rjesner.eng@gmail.com>
2024-08-27 18:50:53 -0400

    feat: MariaDB connection Singleton for multi-module project

--- app/api/dbSetup.py
+++ app/api/dbSetup.py
@@ -1,19 +1,5 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from app.api.dbSetupSingleton import dbSetupSingleton
 
-
-DATABASE_URL = "mariadb+mariadbconnector://root:pass123@192.168.99.101:3306/tradingbot"
-engine = create_engine(DATABASE_URL)
-
-
-sql_file_path = 'app/api/dbSetup.sql'
-with open(sql_file_path, 'r') as file:
-    create_table_sql = file.read()
-with engine.connect() as connection:
-    connection.execute(text(create_table_sql))
-
-
-Base = declarative_base()
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+engine = dbSetupSingleton.getEngine()
+Base = dbSetupSingleton.getBase()
+SessionLocal = dbSetupSingleton.getSession()
---
+++ app/api/dbSetupSingleton.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+class dbSetupSingleton:
+	__engine = None
+	__base = None
+	__session = None
+
+	@classmethod
+	def getEngine(cls):
+		if not cls.__engine:
+			DATABASE_URL = "mariadb+mariadbconnector://root:pass123@192.168.99.101:3306/tradingbot"
+			cls.__engine = create_engine(DATABASE_URL)
+
+			sql_file_path = 'app/api/dbSetup.sql'
+			with open(sql_file_path, 'r') as file:
+				create_table_sql = file.read()
+			with cls.__engine.connect() as connection:
+				connection.execute(text(create_table_sql))
+		return cls.__engine
+
+	@classmethod
+	def getBase(cls):
+		if not cls.__base:
+			cls.__base = declarative_base()
+		return cls.__base
+
+	@classmethod
+	def getSession(cls):
+		if not cls.__session:
+			cls.__session = sessionmaker(autocommit=False, autoflush=False, bind=cls.__engine)
+		return cls.__session
