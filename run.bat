d3a55bead16c2604ea2d460b75438155788e210f
Rodrigo Jesner Silva Trindade <rjesner.eng@gmail.com>
2024-09-05 11:06:37 -0400

    build: added project path variable to the Batch script

--- run.bat
+++ run.bat
@@ -1,14 +1,15 @@
 @echo off
+set PROJECT_DIR=%~dp0
 echo .:: Crypto Signal Robot - Shell Script .::
-rmdir /s /q app\static > NUL 2>&1
-mkdir app\static
-rmdir /s /q app\frontend\build > NUL 2>&1
-if exist "app\frontend\node_modules" (
-    cd app\frontend
+rmdir /s /q %PROJECT_DIR%\app\static > NUL 2>&1
+mkdir %PROJECT_DIR%\app\static
+rmdir /s /q %PROJECT_DIR%\app\frontend\build > NUL 2>&1
+if exist "%PROJECT_DIR%\app\frontend\node_modules" (
+    cd %PROJECT_DIR%\app\frontend
     echo [Status] Building program...
     call npm run build > NUL 2>&1
 ) else (
-    cd app\frontend
+    cd %PROJECT_DIR%\app\frontend
     echo [Status] Creating React scripts...
     call npm install react-scripts --save > NUL 2>&1
     echo [Status] Building program...
@@ -17,4 +18,4 @@ if exist "app\frontend\node_modules" (
 echo [Status] Returning to initial directory...
 cd ..\..
 echo [Status] Running Python server...
-%PYTHON_PATH%\python.exe run.py
+%PYTHON_PATH%\python.exe %PROJECT_DIR%\run.py
