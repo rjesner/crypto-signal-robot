bddcc7d1d7ebf6a4524e8e38984a14fa0960736d
Rodrigo Jesner Silva Trindade <rjesner.eng@gmail.com>
2024-09-09 17:02:28 -0400

    feat: URL access redirection to the login page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
 
 from app.main.controllers import IndexPageController
 
@@ -6,5 +6,15 @@ main = Blueprint('main', __name__,
                  static_url_path='',
                  static_folder='../static',
                  template_folder='../static')
+
+
+@main.route('/login')
+@main.route('/signup')
+@main.route('/access')
+@main.route('/profile')
+@main.route('/robot')
+def redirect_to_root():
+    return redirect(url_for('main.index'), code=301)  # Permanent redirect
+
 
 main.add_url_rule("/", view_func=IndexPageController.as_view("index"))
